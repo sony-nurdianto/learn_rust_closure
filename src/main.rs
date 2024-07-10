3ef865b22ae849d306f7fd195035f37b63ce3e46
sony nurdianto <sonynurdianto445@gmail.com>
2024-07-10 13:33:28 +0700

    example of using clousure

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
 fn main() {
-    println!("Hello, world!");
+    let mut data = String::from("original data");
+
+    let closure1 = || {
+        println!("Value of data in closure1: {}", data); // Borrowing data (default)
+    };
+
+    closure1(); // This works because data is still valid
+
+    let mut closure2 = move || {
+        data.push_str(" (modified)"); // Modify data using move
+        println!("Value of data in closure2: {}", data);
+    };
+
+    closure2(); // This now works because closure2 owns data
+
+    // println!("Original data: {}", data); this will be error because we move the ownership of
+    // data to closure2
 }
